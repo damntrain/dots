45a85bca1c456717dcc082d0129727d627b570c1
apophis <apophis@localhost.localdomain>
2022-05-27 15:21:41 +0300

    Updated autinstall script

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,5 +1,15 @@
-#!/usr/bin/env sh
-repodir="$( dirname -- "$0"; )"
-cp -r $repodir/{Documents,.local,.config,.zshrc} $HOME
-fc-cache -f
-dconf load /org/gnome/terminal/legacy/ < $repodir/gnome-terminal.dconf
+#!/usr/bin/env zsh
+print "Be aware that running this script will destroy your existing setup (your .zshrc, neofetch config, gnome-terminal preferences, and so on) and will install ubuntu fonts, it's not made to be interactive, it's made to recreate my setup as simply as possible"
+read -p "Can i put my balls in your jaws? (y/n) " yn
+case $yn in
+	y )	repodir="$( dirname -- "$0"; )"
+		print "Putting things where they should be"
+		cp -r $repodir/{Documents,.local,.config,.zshrc} $HOME
+		print "Updating fonts"
+		fc-cache -f
+		print "applying gnome-terminal settings, be aware that it will destroy your current gnome-terminal settings"
+		dconf load /org/gnome/terminal/legacy/ < $repodir/gnome-terminal.dconf
+		print "Everything done, enjoy 🥒"
+	n )	print "Sadje :<"
+	* )	print "Sadje :<"
+esac
