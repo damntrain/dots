b43291cf4bfa31e96bfd1859db650cc934831061
damntrain <51455723+damntrain@users.noreply.github.com>
2022-09-10 03:49:04 +0300

    Prompt feature-update
    Replaced old symbol used in containerized environments to distribution logo

--- .local/share/zsh/1-prompt.plugin.zsh
+++ .local/share/zsh/1-prompt.plugin.zsh
@@ -1,9 +1,23 @@
 local char='>'
 if [[ "$container" != "" ]]
-then 
-	local char2="%K{magenta}%F{black} ⬣  %k%f"
+then
+	getlogo () # You need nerd-fonts to be able to see this
+	{
+		source /etc/os-release
+		case $ID in
+			arch) logo=;;
+			fedora) logo=;;
+			centos) logo=;;
+			alpine) logo=;;
+			debian) logo=;;
+			opensuse*) logo=;;
+			ubuntu) logo=;;
+		esac
+		printf $logo
+	}
+	logo=$(getlogo)
+	local char2="%K{magenta}%F{black} $logo  %k%f"
 fi
-
 if [[ "$TMUX" == "" ]]
 then
 	local pth='%K{white}%F{black} %~ %k%f'
